8a27bf8fc5b008ae97630410c367a478c6bd267f
AnastasiaBuglakova <bugla2005@mail.ru>
2022-10-15 14:33:52 +0300

    Last task done

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ a = 5; b = 7 ->  max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
+/*
 Console.WriteLine("Please, enter first number");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Please, enter second number");
@@ -15,4 +16,32 @@ if (a > b)
 else 
 {
     Console.WriteLine("Max=" + b);
+}
+*/
+/***Задача 4:** Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+/*
+Console.WriteLine("Please, enter first number:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please, enter second number:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please, enter third number:");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a < b) max = b;
+if (c > b) max = c;
+Console.WriteLine("Max=" + max);*/
+
+/* Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
+// int Result = Num % 10;
+Console.WriteLine("Please, enter number:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <= N) 
+{
+    Console.Write(i + " ");
+    i+=2;
 }
